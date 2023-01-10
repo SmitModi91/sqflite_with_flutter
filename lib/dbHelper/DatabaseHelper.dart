7e4d9ce90d7230943d33e212cc2d8bcfe51c0e69
modismit2 <modismit2@gmail.com>
2023-01-10 20:55:33 +0530

    Check insert, update, query , delete method with Sqflite in demo

---
+++ lib/dbHelper/DatabaseHelper.dart
@@ -0,0 +1,91 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+
+class DatabaseHelper {
+  static const _databaseName = "MySqfliteDemo.db";
+  static const _databaseVersion = 1;
+
+  static const table = 'my_table';
+
+  static const columnId = '_id';
+  static const columnName = 'name';
+  static const columnAge = 'age';
+
+  // Create database object
+  late Database _db;
+
+  //Initialize Database
+  Future<void> init() async {
+    final documentsDirectory = await getApplicationDocumentsDirectory();
+    final path = join(documentsDirectory.path, _databaseName);
+    _db = await openDatabase(
+      path,
+      version: _databaseVersion,
+      onCreate: _onCreate,
+    );
+  }
+
+  // Oncreate method with table creation in database
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+          CREATE TABLE $table (
+            $columnId INTEGER PRIMARY KEY,
+            $columnName TEXT NOT NULL,
+            $columnAge INTEGER NOT NULL
+          )
+          ''');
+  }
+
+
+// Helper methods
+
+// Inserts a row in the database where each key in the Map is a column name
+// and the value is the column value. The return value is the id of the
+// inserted row.
+
+  Future<int> insert(Map<String, dynamic> row) async {
+    return await _db.insert(table, row);
+  }
+
+
+// All of the rows are returned as a list of maps, where each map is
+// a key-value list of columns.
+
+  Future<List<Map<String,dynamic>>> queryAllRows() async {
+    return await _db.query(table);
+  }
+
+
+
+ // All of the methods (insert, query, update, delete) can also be done using
+// raw SQL commands. This method uses a raw query to give the row count.
+
+  Future<int> queryRowCount() async {
+    final results = await _db.rawQuery('SELECT COUNT(*) FROM $table');
+    return Sqflite.firstIntValue(results) ?? 0;
+  }
+
+// We are assuming here that the id column in the map is set. The other
+// column values will be used to update the row.
+
+  Future<int> update(Map<String, dynamic> row) async {
+    int id = row[columnId];
+    return await _db.update(
+      table,
+      row,
+      where: '$columnId = ?',
+      whereArgs: [id],
+    );
+  }
+
+  // Deletes the row specified by the id. The number of affected rows is
+  // returned. This should be 1 as long as the row exists.
+  Future<int> delete(int id) async {
+    return await _db.delete(
+      table,
+      where: '$columnId = ?',
+      whereArgs: [id],
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'dbHelper/DatabaseHelper.dart';
 
-void main() {
-  runApp(MaterialApp(
+// Here we are using a global variable. You can use something like
+// get_it in a production app.
+final dbHelper = DatabaseHelper();
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dbHelper.init();
+  runApp(const MaterialApp(
     home: Home(),
   ));
-
 }
 
 class Home extends StatefulWidget {
@@ -19,11 +25,104 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text( "SqfLiteDemo"),
+        title: const Text("SqfLiteDemo"),
         centerTitle: true,
         elevation: 0,
       ),
+      body: Stack(children: [
+        const Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Card(
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Text(
+                'Welcome to SqFlite Demo for beginners',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 22,
+                ),
+              ),
+            ),
+            borderOnForeground: true,
+            elevation: 2,
+            color: Colors.amberAccent,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            margin: EdgeInsets.all(8),
+
+          ),
+        ),
+        Container(
+          alignment: Alignment.center,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  _insert();
+                },
+                child: const Text('insert Value'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _query();
+                },
+                child: const Text('Query Value'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _update();
+                },
+                child: const Text('Update Value'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  _delete();
+                },
+                child: const Text('Delete Value'),
+              ),
+            ],
+          ),
+        ),
+      ]),
     );
   }
 }
 
+// Button onPressed methods
+
+void _insert() async {
+  // row to insert
+  Map<String, dynamic> row = {
+    DatabaseHelper.columnName: 'Smit Modi',
+    DatabaseHelper.columnAge: 32
+  };
+  final id = await dbHelper.insert(row);
+  debugPrint('inserted row id: $id');
+}
+
+void _query() async {
+  final allRows = await dbHelper.queryAllRows();
+  debugPrint('query all rows:');
+  for (final row in allRows) {
+    debugPrint(row.toString());
+  }
+}
+
+void _update() async {
+  // row to update
+  Map<String, dynamic> row = {
+    DatabaseHelper.columnId: 1,
+    DatabaseHelper.columnName: 'Mary',
+    DatabaseHelper.columnAge: 32
+  };
+  final rowsAffected = await dbHelper.update(row);
+  debugPrint('updated $rowsAffected row(s)');
+}
+
+void _delete() async {
+  // Assuming that the number of rows is the id for the last row.
+  final id = await dbHelper.queryRowCount();
+  final rowsDeleted = await dbHelper.delete(id);
+  debugPrint('deleted $rowsDeleted row(s): row $id');
+}
